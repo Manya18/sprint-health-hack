2845f04f2a87839e0f43882bfc879c98da187c90
bl4ckfl4me-dev <bl4ckfl4me-dev@yandex.ru>
2024-11-16 00:19:50 +0400

    added cors middleware

--- backend/src/server/__init__.py
+++ backend/src/server/__init__.py
@@ -1,10 +1,21 @@
 from .performance_analysis import performance_router
+from fastapi.middleware.cors import CORSMiddleware
 from .bottlenecks import bottlenecks_router
 from .charts import charts_router
 from fastapi import FastAPI
 
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(performance_router)
 app.include_router(bottlenecks_router)
 app.include_router(charts_router)
