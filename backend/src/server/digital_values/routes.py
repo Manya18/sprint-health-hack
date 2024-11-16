44db01f4958eeca20bc19a7d191c11718b03059b
digischh <gavrilovadiana28@gmail.com>
2024-11-17 01:28:27 +0400

    task_duplicate add

--- backend/src/server/digital_values/routes.py
+++ backend/src/server/digital_values/routes.py
@@ -37,7 +37,7 @@ def get_to_do_tasks(
         entity_ids_str = ', '.join(map(str, entity_ids_list))        
         
         query = text(f"""
-            SELECT SUM(estimation) FROM Task
+            SELECT SUM(estimation) FROM task_duplicate
             WHERE entity_id IN ({entity_ids_str}) AND status='Создано' {areas_condition}
         """)
         
@@ -79,7 +79,7 @@ def get_in_work_tasks(
         entity_ids_str = ', '.join(map(str, entity_ids_list))
         
         query = text(f"""
-            SELECT SUM(estimation) FROM Task
+            SELECT SUM(estimation) FROM task_duplicate
             WHERE entity_id IN ({entity_ids_str}) AND status NOT IN ('Сделано', 'Снято') {areas_condition}
         """)
         
@@ -121,7 +121,7 @@ def get_close_tasks(
         entity_ids_str = ', '.join(map(str, entity_ids_list))
         
         query = text(f"""
-            SELECT SUM(estimation) FROM Task
+            SELECT SUM(estimation) FROM task_duplicate
             WHERE entity_id IN ({entity_ids_str}) AND type IN ('История', 'Задача', 'Дефект') AND status IN('Закрыто', 'Выполнено') {areas_condition}
         """)
         
@@ -163,7 +163,7 @@ def get_cancel_tasks(
         entity_ids_str = ', '.join(map(str, entity_ids_list))
         
         query = text(f"""
-            SELECT SUM(estimation) FROM Task
+            SELECT SUM(estimation) FROM task_duplicate
             WHERE entity_id IN ({entity_ids_str}) AND status IN ('Закрыто', 'Выполнено') AND resolution IN('Отклонено', 'Отменено инициатором', 'Дубликат') {areas_condition} OR entity_id IN ({entity_ids_str}) AND type='Дефект' AND status='Отклонен исполнителем' {areas_condition}
         """)
         
@@ -174,5 +174,130 @@ def get_cancel_tasks(
             'sprint_name': item['sprint_name'],
             'estimation': round(estimation / 3600, 1) if estimation is not None else 0
         })
-
+    
     return result
+
+@digital_values_router.post('/update_task_duplicate')
+def update_task_duplicate(
+    sprint_names: list[str] = Query(...), 
+    start_date: str = String,          
+    end_date: str = String,            
+    timeline: str = String
+):
+    sprint_names_str = ', '.join(f"'{name}'" for name in sprint_names)
+
+    query = text(f"""DELETE FROM task_duplicate""")
+    with engine.connect() as connection:
+        connection.execute(query)
+
+    # Подготовка запроса
+    query = text(f"""
+        WITH task_status_history AS (
+            SELECT
+                h.entity_id,
+                h.history_change,
+                h.history_change_type,
+                h.history_date,
+                ROW_NUMBER() OVER (PARTITION BY h.entity_id ORDER BY h.history_date DESC) AS row_num
+            FROM history h
+            WHERE h.history_property_name = 'Статус'
+              AND h.history_date BETWEEN '{start_date}'::date AND '{end_date}'::date
+        ),
+        sprint_tasks AS (
+            SELECT
+                s.sprint_name,
+                s.sprint_status,
+                s.entity_ids
+            FROM sprint s
+            WHERE s.sprint_name IN ({sprint_names_str})   -- Используем IN для передачи нескольких значений
+              AND s.sprint_end_date >= '{timeline}'::date
+        ),
+        task_info AS (
+            SELECT
+                t.entity_id,
+                t.area,
+                t.type,
+                t.status AS original_status,
+                t.state,
+                t.priority,
+                t.ticket_number,
+                t.name,
+                t.create_date,
+                t.created_by,
+                t.update_date,
+                t.updated_by,
+                t.parent_ticket_id,
+                t.assignee,
+                t.owner,
+                t.due_date,  
+                t.rank,
+                t.estimation,
+                t.spent,
+                t.workgroup,
+                t.resolution,
+                th.history_change AS last_status,
+                th.history_change_type,
+                th.history_date AS start_date
+            FROM task t
+            LEFT JOIN task_status_history th
+                ON t.entity_id = th.entity_id
+                AND th.row_num = 1
+        )
+
+        INSERT INTO task_duplicate (
+            entity_id,
+            area,
+            type,
+            status,
+            state,
+            priority,
+            ticket_number,
+            name,
+            create_date,
+            created_by,
+            update_date,
+            updated_by,
+            parent_ticket_id,
+            assignee,
+            owner,
+            due_date,
+            rank,
+            estimation,
+            spent,
+            workgroup,
+            resolution
+        )
+        SELECT
+            ti.entity_id,
+            ti.area,
+            ti.type,
+            COALESCE(ti.last_status, ti.original_status) AS status,
+            ti.state,
+            ti.priority,
+            ti.ticket_number,
+            ti.name,
+            ti.create_date,
+            ti.created_by,
+            ti.update_date,
+            ti.updated_by,
+            ti.parent_ticket_id,
+            ti.assignee,
+            ti.owner,
+            ti.due_date,
+            ti.rank,
+            ti.estimation,
+            ti.spent,
+            ti.workgroup,
+            ti.resolution
+        FROM sprint_tasks st
+        LEFT JOIN task_info ti
+            ON ti.entity_id = ANY(st.entity_ids)
+        WHERE st.sprint_name IN ({sprint_names_str})  -- Используем IN для нескольких значений
+          AND ti.due_date <= '{end_date}'::date
+        ORDER BY ti.entity_id;
+    """)
+
+    with engine.connect() as connection:
+        connection.execute(query)
+
+    return {"status": "success", "message": "Tasks inserted into task_duplicate"}
