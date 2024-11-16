de9071d34dd8412c4bca05e6b539784770f36846
dariadariadariadariadaria <149558714+dariadariadariadariadaria@users.noreply.github.com>
2024-11-16 17:53:05 +0400

    get to_do_tasks, in_work_tasks, cancel_tasks, close_tasks

--- backend/src/server/digital_values/routes.py
+++ backend/src/server/digital_values/routes.py
@@ -11,13 +11,11 @@ from fastapi import APIRouter, Query
 from sqlalchemy.dialects.postgresql import ARRAY
 from . import digital_values_router
 
-@digital_values_router.get('/get_estimation')
-def get_estimation_sum(
-    sprint_names: list[str] = Query(...), 
-    statuses: list[str] = Query(...)
+@digital_values_router.get('/get_to_do_tasks')
+def get_to_do_tasks(
+    sprint_names: list[str] = Query(...)
 ):
     sprint_names_str = ', '.join(f"'{name}'" for name in sprint_names)
-    
     query = text(f"""
         SELECT UNNEST(entity_ids) FROM sprint
         WHERE sprint_name IN ({sprint_names_str})
@@ -26,15 +24,10 @@ def get_estimation_sum(
     with engine.connect() as connection:
         entity_ids = connection.execute(query).scalars().all()
 
-    if not entity_ids:
-        return {"error": "Для данных спринтов нет задач"}
-
     entity_ids_str = ', '.join(map(str, entity_ids))
-    statuses_str = ', '.join(f"'{status}'" for status in statuses)
-
     query = text(f"""
         SELECT SUM(estimation) FROM Task
-        WHERE entity_id IN ({entity_ids_str}) AND status IN ({statuses_str})
+        WHERE entity_id IN ({entity_ids_str}) AND status='Создано'
     """)
 
     with engine.connect() as connection:
@@ -42,13 +35,11 @@ def get_estimation_sum(
     
     return round(result / 3600, 1)
 
-@digital_values_router.get('/get_estimation_exception')
-def get_estimation_sum(
-    sprint_names: list[str] = Query(...), 
-    statuses: list[str] = Query(...)
+@digital_values_router.get('/get_in_work_tasks')
+def get_in_work_tasks(
+    sprint_names: list[str] = Query(...)
 ):
     sprint_names_str = ', '.join(f"'{name}'" for name in sprint_names)
-    
     query = text(f"""
         SELECT UNNEST(entity_ids) FROM sprint
         WHERE sprint_name IN ({sprint_names_str})
@@ -57,15 +48,58 @@ def get_estimation_sum(
     with engine.connect() as connection:
         entity_ids = connection.execute(query).scalars().all()
 
-    if not entity_ids:
-        return {"error": "Для данных спринтов нет задач"}
-
     entity_ids_str = ', '.join(map(str, entity_ids))
-    statuses_str = ', '.join(f"'{status}'" for status in statuses)
-
     query = text(f"""
         SELECT SUM(estimation) FROM Task
-        WHERE entity_id IN ({entity_ids_str}) AND status NOT IN ({statuses_str})
+        WHERE entity_id IN ({entity_ids_str}) AND status NOT IN ('Сделано', 'Снято')
+    """)
+
+    with engine.connect() as connection:
+        result = connection.execute(query).scalar()
+    
+    return round(result / 3600, 1)
+
+@digital_values_router.get('/get_close_tasks')
+def get_close_tasks(
+    sprint_names: list[str] = Query(...)
+):
+    sprint_names_str = ', '.join(f"'{name}'" for name in sprint_names)
+    query = text(f"""
+        SELECT UNNEST(entity_ids) FROM sprint
+        WHERE sprint_name IN ({sprint_names_str})
+    """)
+
+    with engine.connect() as connection:
+        entity_ids = connection.execute(query).scalars().all()
+
+    entity_ids_str = ', '.join(map(str, entity_ids))
+    query = text(f"""
+        SELECT SUM(estimation) FROM Task
+        WHERE entity_id IN ({entity_ids_str}) AND type IN ('История', 'Задача', 'Дефект') AND status IN('Закрыто', 'Выполнено')
+    """)
+
+    with engine.connect() as connection:
+        result = connection.execute(query).scalar()
+    
+    return round(result / 3600, 1)
+
+@digital_values_router.get('/get_cancel_tasks')
+def get_cancel_tasks(
+    sprint_names: list[str] = Query(...)
+):
+    sprint_names_str = ', '.join(f"'{name}'" for name in sprint_names)
+    query = text(f"""
+        SELECT UNNEST(entity_ids) FROM sprint
+        WHERE sprint_name IN ({sprint_names_str})
+    """)
+
+    with engine.connect() as connection:
+        entity_ids = connection.execute(query).scalars().all()
+
+    entity_ids_str = ', '.join(map(str, entity_ids))
+    query = text(f"""
+        SELECT SUM(estimation) FROM Task
+        WHERE entity_id IN ({entity_ids_str}) AND status IN ('Закрыто', 'Выполнено') AND resolution IN('Отклонено', 'Отменено инициатором', 'Дубликат') OR type='Дефект' AND status='Отклонен исполнителем'
     """)
 
     with engine.connect() as connection:
