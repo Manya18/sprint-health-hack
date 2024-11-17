03c1b26a7c66016538de40a736ebb371b8b2dc8f
digischh <gavrilovadiana28@gmail.com>
2024-11-17 17:31:15 +0400

    toast

--- frontend/src/components/filesUpload/FilesUpload.tsx
+++ frontend/src/components/filesUpload/FilesUpload.tsx
@@ -1,42 +1,43 @@
 import { ChangeEvent, useState } from "react";
 import styles from "./filesUpload.module.css"
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const FilesUpload = () => {
 
     const handleChange = async (event: any, tableName: string) => {
         const formData = new FormData();
         formData.append("file", event.target.files[0]);
-    
+
         try {
             const response = await axios.post(
-            `http://localhost:8000/${tableName}`,
-            formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            }
+                `http://localhost:8000/${tableName}`,
+                formData,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
             );
-            console.log(response.data.message);
+            toast.success('Файл загружен');
         } catch (error) {
-            console.error("Ошибка при загрузке файла:", error);
+            toast.warn('Ошибка при загрузке файла');
         }
     }
-    
+
     return (
         <div className={styles.FilesUpload}>
             <div className={styles.inputWrapper}>
                 <label className={styles.label}>Загрузите файл задач</label>
-                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_data_file')}/>
+                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_data_file')} />
             </div>
             <div className={styles.inputWrapper}>
                 <label className={styles.label}>Загрузите файл спринтов</label>
-                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_sprint_file')}/>
+                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_sprint_file')} />
             </div>
             <div className={styles.inputWrapper}>
                 <label className={styles.label}>Загрузите файл истории</label>
-                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_history_file')}/>
+                <input className={styles.input} type='file' onChange={(event) => handleChange(event, 'upload_history_file')} />
             </div>
         </div>
     )
